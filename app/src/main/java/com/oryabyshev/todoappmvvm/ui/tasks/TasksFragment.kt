c5f6d08715b6900f096c09909f4e5288f7f0917b
OlegRyabyshev <oryabyshev@gmail.com>
2021-04-13 16:26:35 +0300

    setting up components

---
+++ app/src/main/java/com/oryabyshev/todoappmvvm/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.oryabyshev.todoappmvvm.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.oryabyshev.todoappmvvm.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
